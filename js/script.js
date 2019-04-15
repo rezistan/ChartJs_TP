5699a2ac68ecda4e7638490c5245c7993423f93d
Rezistan <taslas@hotmail.fr>
2019-04-15 17:40:56 +0200

    dataset index bug fix bar

--- js/script.js
+++ js/script.js
@@ -285,10 +285,9 @@ var sliceClickHandler = function(e, args) {
                 data = ci.data.datasets[datasetIndex].data[index]; //data
                 label = ci.data.datasets[datasetIndex].label[index]; //label
             }
-            else{
-                //console.log(element);
-                //console.log(args[0]);
-                datasetIndex = element._datasetIndex;
+            else{ //bar
+                datasetIndex = ci.getDatasetAtEvent(e)[0]._datasetIndex;
+                console.log(ci.getDatasetAtEvent(e)[datasetIndex]); ////////////////////////////////afficher taille
                 data = ci.data.datasets[datasetIndex].data[index]; //data
                 label = ci.data.datasets[datasetIndex].label; //label
             }
